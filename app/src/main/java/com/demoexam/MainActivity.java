e211b3efec6dcbd128301b21b44a96115c5e5bb6
mobilean <rohit@vishumangal.com>
2023-01-20 12:30:27 +0530

    All New Projecdt

---
+++ app/src/main/java/com/demoexam/MainActivity.java
@@ -0,0 +1,22 @@
+package com.demoexam;
+
+import com.facebook.react.ReactActivity;
+import android.view.WindowManager;
+import android.os.Bundle;
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "DemoExam";
+  }
+
+ 
+ getWindow().setFlags(
+       WindowManager.LayoutParams.FLAG_SECURE,
+      WindowManager.LayoutParams.FLAG_SECURE
+     ); 
+}
